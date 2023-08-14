ee5618cf1c78e7dd8032c7c296f8e47e39785e93
Electro_593 <electro5939@gmail.com>
2023-08-13 21:21:21 -0400

    It kind of loads...

---
+++ .gdbinit
@@ -0,0 +1,29 @@
+set pagination off
+
+define wc_print
+  echo "
+  set $c = (c16*)$arg0
+  while(*$c)
+    if(*$c > 0x7f)
+      printf "[%x]", *$c
+    else
+      printf "%c", *$c
+    end
+    set $c++
+  end
+  echo "\n
+end
+
+info files
+file
+add-symbol-file ./build/kernel 0x1000
+
+set architecture i386:x86-64:intel
+set disassembly-flavor intel
+target remote :1234
+
+break Kernel_Entry
+
+layout split
+
+c
---
+++ scripts/build.sh
@@ -0,0 +1,40 @@
+if [ -e "build/loader.so" ]; then
+   ARCH=$(uname -m | sed s,i[3456789]86,ia32,)
+
+
+
+   if [ $ARCH = "aarch64" ]; then
+      EFIARCH="pei-aarch64-little"
+   else
+      EFIARCH="efi-app-${ARCH}"
+   fi
+   SECTIONS="-j .text -j .sdata -j .data -j .dynamic -j .dynsym  -j .rel -j .rela -j .reloc"
+   SECTIONS_DBG="$SECTIONS -j .debug_info -j .debug_abbrev -j .debug_loc -j .debug_aranges -j .debug_line -j .debug_macinfo -j .debug_str -j .debug_line_str"
+   objcopy $SECTIONS_DBG --target $EFIARCH --subsystem=10 build/loader.so build/loader_dbg
+   objcopy $SECTIONS     --target $EFIARCH --subsystem=10 build/loader.so build/loader
+   objcopy -R .note* -R .comment build/kernel.so build/kernel
+
+
+
+   objdump -l -S -d --source-comment build/loader.so > build/loader.s
+   objdump -l -S -d --source-comment build/kernel > build/kernel.s
+   objdump --all-headers build/loader_dbg > build/loader.dump
+   objdump --all-headers build/kernel > build/kernel.dump
+
+
+
+   find build/ -name "*.o"  | xargs rm 2>/dev/null
+   find build/ -name "*.so" | xargs rm 2>/dev/null
+
+
+
+   IsMounted=$(mount | grep '/mnt')
+   if [ "$IsMounted" = "" ]; then
+      sudo qemu-nbd -c /dev/nbd0 emulator/disk.vhd
+      sudo mount -t auto -o rw /dev/nbd0p1 /mnt
+   fi
+   sudo cp build/loader /mnt/EFI/BOOT/BOOTX64.efi
+   sudo cp build/kernel /mnt/kernel
+   sudo umount /dev/nbd0p1
+   sudo qemu-nbd -d /dev/nbd0
+fi
