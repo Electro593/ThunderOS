5789d68b1b0a142761c71efe3b3844365b4599af
Electro_593 <electro5939@gmail.com>
2026-01-11 18:17:16 -0600

    Remove some old emulator code

--- scripts/run.bat
+++
@@ -1,4 +0,0 @@
-@REM call "C:/Program Files/qemu/qemu-system-x86_64" -bios D:\Programming\C\ThunderOS\virtualbox\OVMF-pure-efi.fd
-call "C:/Program Files/qemu/qemu-system-x86_64" -S -cpu qemu64 -bios D:\Programming\C\ThunderOS\virtualbox\OVMF-pure-efi.fd
-@REM call "C:/Program Files/qemu/qemu-system-x86_64" -cpu qemu64 -bios D:\Programming\C\ThunderOS\virtualbox\OVMF-pure-efi.fd -drive file=build/OS.efi
-@REM call "C:/Program Files/qemu/qemu-system-x86_64" -drive file=x86_64.img,format=raw
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# konsole -e "gdb build/loader_dbg" &
-# konsole -e "gdb" &
-
 echo Starting qemu...
 
 qemu-system-x86_64 -cpu qemu64 \
-                   -bios ./emulator/OVMF.fd \
+                   -bios ./scripts/ovmf.fd \
                    -drive if=ide,file=./build/disk.qcow2 \
                    -device qemu-xhci,id=xhci \
                    -m 1G \
